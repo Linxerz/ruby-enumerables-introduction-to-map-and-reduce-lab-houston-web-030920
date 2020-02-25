d4b2f171c4a41952eea2d700691fdd7328a74b30
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-25 23:02:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,5 +20,5 @@ def reduce_to_total(source_array,starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.inject(0){|i| if i? true : false}
+  source_array.inject(0){|i| if i? then true else false end}
 end
