26788c84758f8a9c517ad1877029c042b05a1c1d
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-25 23:07:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,3 +25,7 @@ def reduce_to_all_true(source_array)
   source_array.inject(0){|i| if i!= true then false else true end}
     pp source_array
 end
+
+def reduce_to_any_true(source_array)
+  source_array.any?
+end
